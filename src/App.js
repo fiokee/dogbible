8493f54758ffc59392494dfc399cd83d7cc44eed
fiokee <smleffiong@gmail.com>
2023-02-16 16:23:08 +0100

    updating

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Navigation from './routes/navigations/navigation';
 import Home from './routes/home/home';
-
+import SignIn from './routes/signIn/signIn'
 function App() {
   const Shop =()=>{
     return(
--- src/routes/home/home.js
+++ src/routes/home/home.js
@@ -5,7 +5,7 @@ import { Outlet } from 'react-router-dom'
 const home = () => {
   return (
     <div>
-      <Outlet/>
+      {/* <Outlet/> */}
     </div>
   )
 }

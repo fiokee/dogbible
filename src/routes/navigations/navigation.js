313cc3d2d5f8f2a79c78e85a78059ea889fafeb2
fiokee <smleffiong@gmail.com>
2023-02-17 20:09:58 +0100

    styling the navbar

--- src/routes/navigations/navigation.js
+++ src/routes/navigations/navigation.js
@@ -1,15 +1,17 @@
-import React from 'react'
-import { Fragment } from 'react'
-import { Outlet, Link } from 'react-router-dom'
+import React from 'react';
+import { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
   return (
     <Fragment>
       <div className="flex justify-center space-x-4 bg-yellow-500">
         <Link className="logo-container" to='/'>
-        <h1 className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Logo</h1>
+        <h1 className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900 justify-end absolute left-5">Logo</h1>
         </Link>
-        
+          <Link className='font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900' to='/'>
+            Home
+          </Link>
           <Link className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900" to='shop'>
             SHOP
           </Link>
@@ -20,8 +22,6 @@ const Navigation = () => {
             REGISTER 
           </Link>
         </div>
-      
-     
     </Fragment>
   )
 }

180364a3cc7e6b6323299b6cbb153a055884db6e
fiokee <smleffiong@gmail.com>
2023-02-18 15:59:39 +0100

    updating the categories of dogs

--- src/components/shopData.js
+++ src/components/shopData.js
@@ -59,7 +59,40 @@ var breeds = [
         "photo": "https://images.pexels.com/photos/4588007/pexels-photo-4588007.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         "Price": 200,
       },
+
+      {
+        "id": 8,
+        "name": "Akita-ken",
+        "photo": "",
+        "Price": 200,
+      },
     ],
+    },
+
+    {
+       title: 'Husky',
+       items: [
+        {
+            "id": 9,
+            "name": "Alaskan Malamute",
+            "photo": "https://images.pexels.com/photos/2853422/pexels-photo-2853422.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            "Price": 200,
+          },
+
+          {
+            "id": 10,
+            "name": "American Klee Klai",
+            "photo": "https://images.pexels.com/photos/3196887/pexels-photo-3196887.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            "Price": 200,
+          },
+
+          {
+            "id": 11,
+            "name": "American Eskimo",
+            "photo": "https://images.pexels.com/photos/3812207/pexels-photo-3812207.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            "Price": 200,
+          },
+       ],
     }
 
 ]

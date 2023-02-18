6202fe08c0db1928dcc26a7a1a3e582822d4b964
fiokee <smleffiong@gmail.com>
2023-02-18 15:46:29 +0100

    working on shop

---
+++ src/components/shopData.js
@@ -0,0 +1,67 @@
+var breeds = [
+
+    {
+     title: 'German Shepherd',
+        items: [
+      {
+        "id": 1,
+        "name": "Saddle Coat",
+        "photo": "https://images.pexels.com/photos/351406/pexels-photo-351406.jpeg",
+        "Price": 200,
+      },
+      {
+        "id": 2,
+        "name": "Black German Shepherd",
+        "photo": "https://images.pexels.com/photos/1800314/pexels-photo-1800314.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "Price": 200,
+      },
+      {
+        "id": 3,
+        "name": "Panda German Shepherd",
+         "photo": "https://images.pexels.com/photos/9830197/pexels-photo-9830197.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "Price": 200,
+      },
+      {
+        "id": 4,
+        "name": "Sable German Shepherd",
+         "photo": "https://images.pexels.com/photos/342214/pexels-photo-342214.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "Price": 200,
+      },
+      {
+        "id": 5,
+        "name": "White German Shepherd",
+        "photo": "https://images.pexels.com/photos/846086/pexels-photo-846086.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "Price": 200,
+      },
+    ],
+    },
+    
+    {
+    title: 'Akita',
+    items: [
+    
+    {
+        "id": 6,
+        "name": "Japanese Akita",
+        "photo": "https://images.pexels.com/photos/5745215/pexels-photo-5745215.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "Price": 200,
+      },
+    {
+        "id": 6,
+        "name": "American Akita",
+        "photo": "https://images.pexels.com/photos/243914/pexels-photo-243914.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "Price": 200,
+      },
+
+      {
+        "id": 7,
+        "name": "Akita Inu",
+        "photo": "https://images.pexels.com/photos/4588007/pexels-photo-4588007.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "Price": 200,
+      },
+    ],
+    }
+
+]
+
+    

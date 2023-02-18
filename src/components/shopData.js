cb30ac33704775984a74abc0983a4a37b90f1d5a
fiokee <smleffiong@gmail.com>
2023-02-18 20:29:15 +0100

    updating the image url

--- src/components/shopData.js
+++ src/components/shopData.js
@@ -6,31 +6,31 @@ var breeds = [
       {
         "id": 1,
         "name": "Saddle Coat",
-        "photo": "https://images.pexels.com/photos/351406/pexels-photo-351406.jpeg",
+        "photo": "https://rb.gy/fah1lz",
         "Price": 200,
       },
       {
         "id": 2,
         "name": "Black German Shepherd",
-        "photo": "https://images.pexels.com/photos/1800314/pexels-photo-1800314.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "photo": "https://rb.gy/50wwwx",
         "Price": 200,
       },
       {
         "id": 3,
         "name": "Panda German Shepherd",
-         "photo": "https://images.pexels.com/photos/9830197/pexels-photo-9830197.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+         "photo": "https://rb.gy/s6xdpe",
         "Price": 200,
       },
       {
         "id": 4,
         "name": "Sable German Shepherd",
-         "photo": "https://images.pexels.com/photos/342214/pexels-photo-342214.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+         "photo": "https://rb.gy/mtmbkr",
         "Price": 200,
       },
       {
         "id": 5,
         "name": "White German Shepherd",
-        "photo": "https://images.pexels.com/photos/846086/pexels-photo-846086.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "photo": "https://rb.gy/m9noqm",
         "Price": 200,
       },
     ],
@@ -43,27 +43,27 @@ var breeds = [
     {
         "id": 6,
         "name": "Japanese Akita",
-        "photo": "https://images.pexels.com/photos/5745215/pexels-photo-5745215.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "photo": "https://rb.gy/96djjt",
         "Price": 200,
       },
     {
         "id": 6,
         "name": "American Akita",
-        "photo": "https://images.pexels.com/photos/243914/pexels-photo-243914.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "photo": "https://rb.gy/hjgtmt",
         "Price": 200,
       },
 
       {
         "id": 7,
         "name": "Akita Inu",
-        "photo": "https://images.pexels.com/photos/4588007/pexels-photo-4588007.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "photo": "https://rb.gy/5csygg",
         "Price": 200,
       },
 
       {
         "id": 8,
         "name": "Akita-ken",
-        "photo": "",
+        "photo": "https://rb.gy/t9hfdh",
         "Price": 200,
       },
     ],
@@ -75,24 +75,56 @@ var breeds = [
         {
             "id": 9,
             "name": "Alaskan Malamute",
-            "photo": "https://images.pexels.com/photos/2853422/pexels-photo-2853422.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            "photo": "https://rb.gy/7wxfhv",
             "Price": 200,
           },
 
           {
             "id": 10,
             "name": "American Klee Klai",
-            "photo": "https://images.pexels.com/photos/3196887/pexels-photo-3196887.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            "photo": "https://rb.gy/ayqyy6",
             "Price": 200,
           },
 
           {
             "id": 11,
             "name": "American Eskimo",
-            "photo": "https://images.pexels.com/photos/3812207/pexels-photo-3812207.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            "photo": "https://rb.gy/9veh2b",
+            "Price": 200,
+          },
+
+          {
+            "id": 12,
+            "name": "White Husky",
+            "photo": "https://rb.gy/mit5lc",
             "Price": 200,
           },
        ],
+    },
+
+    {
+        title: 'PitBull',
+        items:[
+        {
+            "id": 15,
+            "name": "American Pit Bull Terrier",
+            "photo": "https://rb.gy/wbjghh",
+            "Price": 200,
+            },
+
+            {
+              "id": 16,
+              "name": "American Staffordshire Terrier",
+              "photo": "https://rb.gy/y83hvh",
+              "Price": 200,
+            },
+            {
+              "id": 17,
+              "name": "American Bully",
+              "photo": "https://rb.gy/rp0wfm",
+              "Price": 200,
+            },
+        ],
     }
 
 ]

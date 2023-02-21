2098129fd09059264fe749f3ee4978a07ca27c27
fiokee <smleffiong@gmail.com>
2023-02-21 13:33:41 +0100

    current dog

--- src/routes/home/landing/Ourbreed/OurBreed.js
+++ src/routes/home/landing/Ourbreed/OurBreed.js
@@ -1,71 +1,75 @@
 import React from 'react'
+import { GrNotes } from 'react-icons/gr';
+import { GiDogBowl } from 'react-icons/gi';
+import Dogbanner from '../../../../assets/dog-banner.png';
+import Dogfood from '../../../../assets/dog-food.png';
 
 const OurBreed = () => {
   return (
 <div>
       <p className="text-3xl font-bold text-center" >Our Popular Breeds</p>
-    <div class="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 w-full m-6 p-6">
-        <div class="relative overflow-hidden rounded-lg shadow-lg cursor-pointer">
-        <img class="object-cover w-full h-48 brightness-50" src="https://rb.gy/fah1lz" alt="Flower and sky"/>
+    <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 w-full m-6 p-6">
+        <div className="relative overflow-hidden rounded-lg shadow-lg cursor-pointer">
+        <img className="object-cover w-full h-48 brightness-50" src="https://rb.gy/fah1lz" alt="Flower and sky"/>
 
-        <div class="absolute top-0 left-0 px-6 py-4">
-        <h4 class="mb-3 text-xl font-semibold tracking-tight text-white">Saddle Coat</h4>
-        <p class="leading-normal text-gray-100">Males<span className=''>.</span>Females</p>
+        <div className="absolute top-0 left-0 px-6 py-4">
+        <h4 className="mb-3 text-xl font-semibold tracking-tight text-white">Saddle Coat</h4>
+        <p className="leading-normal text-gray-100">Males<span className=''>.</span>Females</p>
         <p className='bg-[lightgreen] text-white text-center rounded-xl mt-10'>Available</p>
         <p className='font-semibold tracking-tight text-white'>10 Weeks old</p>
         </div>
         <h3>Marie Hademan Pahrum, Nv89049</h3>
         </div>
 
-        <div class="relative overflow-hidden rounded-lg shadow-lg cursor-pointer">
-        <img class="object-cover w-full h-48 brightness-50" src="https://rb.gy/9veh2b" alt="Flower and sky"/>
+        <div className="relative overflow-hidden rounded-lg shadow-lg cursor-pointer">
+        <img className="object-cover w-full h-48 brightness-50" src="https://rb.gy/9veh2b" alt="Flower and sky"/>
 
-        <div class="absolute top-0 left-0 px-6 py-4">
-        <h4 class="mb-3 text-xl font-semibold tracking-tight text-white">American Eskimo</h4>
-        <p class="leading-normal text-gray-100">Males<span className=''>.</span>Females</p>
+        <div className="absolute top-0 left-0 px-6 py-4">
+        <h4 className="mb-3 text-xl font-semibold tracking-tight text-white">American Eskimo</h4>
+        <p className="leading-normal text-gray-100">Males<span className=''>.</span>Females</p>
         <p className='bg-[lightgreen] text-white text-center rounded-xl mt-10'>Available</p>
         <p className='font-semibold tracking-tight text-white'>10 Weeks old</p>
         </div>
         <h3>Marie Hademan Pahrum, Nv89049</h3>
         </div>
 
-        <div class="relative overflow-hidden rounded-lg shadow-lg cursor-pointer">
-        <img class="object-cover w-full h-48 brightness-50" src="https://rb.gy/50wwwx" alt="Flower and sky"/>
+        <div className="relative overflow-hidden rounded-lg shadow-lg cursor-pointer">
+        <img className="object-cover w-full h-48 brightness-50" src="https://rb.gy/50wwwx" alt="Flower and sky"/>
 
-        <div class="absolute top-0 left-0 px-6 py-4">
-        <h4 class="mb-3 text-xl font-semibold tracking-tight text-white">Black German Shepherd</h4>
-        <p class="leading-normal text-gray-100">Males<span className=''>.</span>Females</p>
+        <div className="absolute top-0 left-0 px-6 py-4">
+        <h4 className="mb-3 text-xl font-semibold tracking-tight text-white">Black German Shepherd</h4>
+        <p className="leading-normal text-gray-100">Males<span className=''>.</span>Females</p>
         <p className='bg-[lightgreen] text-white text-center rounded-xl mt-10'>Available</p>
         <p className='font-semibold tracking-tight text-white'>10 Weeks old</p>
         </div>
         <h3>Marie Hademan Pahrum, Nv89049</h3>
         </div>
 
-        <div class="relative overflow-hidden rounded-lg shadow-lg cursor-pointer">
-        <img class="object-cover w-full h-48 brightness-50" src="https://rb.gy/ayqyy6" alt="Flower and sky"/>
+        <div className="relative overflow-hidden rounded-lg shadow-lg cursor-pointer">
+        <img className="object-cover w-full h-48 brightness-50" src="https://rb.gy/ayqyy6" alt="Flower and sky"/>
 
-        <div class="absolute top-0 left-0 px-6 py-4">
-        <h4 class="mb-3 text-xl font-semibold tracking-tight text-white">Alaskan Malamute</h4>
-        <p class="leading-normal text-gray-100">Males<span className=''>.</span>Females</p>
+        <div className="absolute top-0 left-0 px-6 py-4">
+        <h4 className="mb-3 text-xl font-semibold tracking-tight text-white">Alaskan Malamute</h4>
+        <p className="leading-normal text-gray-100">Males<span className=''>.</span>Females</p>
         <p className='bg-[lightgreen] text-white text-center rounded-xl mt-10'>Available</p>
         <p className='font-semibold tracking-tight text-white'>10 Weeks old</p>
         </div>
         <h3>Marie Hademan Pahrum, Nv89049</h3>
         </div>
     </div>
-    <div className='container m-auto'>
-      <div>
-      <span>Icon</span>
-      <p>Current news and guids to dog.</p>
-      <img></img>
-      <p>Find out more</p>
+    <div className='container m-auto flex'>
+      <div className='bg-[#ff7f50ea] w-96 rounded-2xl'>
+      <span className='text-3xl font-extrabold'><GrNotes className='m-auto mt-10'/></span>
+      <p className='text-center font-extrabold'>Current news and guids to dog.</p>
+      <p className='text-center p-2 m-7 bg-[orangered] text-white font-semibold rounded-2xl w-auto'>Find out more</p>
+      <img src={Dogbanner} className='w-48 mr-30 relative left-40'></img>
       </div>
-      <div>
-      <span>Icon</span>
-      <p>Current news and guids to dog.</p>
-      <img></img>
-      <p>Find out more</p>
-      </div>
+      {/* <div className=''>
+      <span className='text-3xl'><GiDogBowl/></span>
+      <p className=''>Current news and guids to dog.</p>
+      <img src={Dogfood} className='w-48'></img>
+      <p className=''>Find out more</p>
+      </div> */}
     </div>
 </div>
   )
--- src/routes/navigations/navigation.js
+++ src/routes/navigations/navigation.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import { GiSittingDog } from 'react-icons/gi';
 
 const Navigation = () => {
   return (
     <Fragment>
       <div className="flex justify-center space-x-4 bg-light-500 my-7 text-xl">
         <Link className="logo-container" to='/'>
-        <h1 className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900 justify-end absolute left-5">Logo</h1>
+        <span className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900 justify-end absolute left-5 text-3xl"><GiSittingDog/></span>
         </Link>
           <Link className='font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900' to='/'>
             Home

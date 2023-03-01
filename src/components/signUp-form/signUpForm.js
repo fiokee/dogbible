a8f2579f06bdaf7267bea7caad2e90621a6072a5
fiokee <smleffiong@gmail.com>
2023-03-01 20:42:09 +0100

    updating form and setting the state for input fields

--- src/components/signUp-form/signUpForm.js
+++ src/components/signUp-form/signUpForm.js
@@ -1,37 +1,79 @@
+import React, { useState } from 'react';
 
-import {useState} from 'react'
-
-const signUpForm = () => {
-  
+const SignUpForm = () => {
   const defaultForm = {
     displayName: '',
     name: '',
     email: '',
     password: '',
-    confirmePassword: '',
-  }
+    confirmPassword: '',
+  };
 
-  // const [formFields, setFormFields]= useState(defaultForm);
-  // const {displayName, name, email, password, confirmePassword} = formFields
+  const [formFields, setFormFields] = useState(defaultForm);
+  const { displayName, name, email, password, confirmPassword } = formFields;
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // handle form submission logic
+  };
 
   return (
-    <div>
-      Register
-      <form>
-      <label>displayName</label>
-      <input type='text' name='displayname' value=''></input>
-      <label>Name</label>
-      <input type='text' name='name' value=''></input>
-      <label>email</label>
-      <input type='email' name='name' value=''></input>
-      <label>password</label>
-      <input type='password' name='name' value=''></input>
-      <label>confim password</label>
-      <input type='password' name='name' value=''></input>
-      </form>
-      <button type='submit'>SignUp</button>
-    </div>
-  )
-}
+    <div className='ontainer max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2'>
+      <div className='bg-white px-6 py-8 rounded shadow-md text-black w-full'>
+      <h2 className='mb-8 text-3xl text-center'>Register</h2>
+      <form onSubmit={handleSubmit}>
+        <label>displayName</label>
+        <input
+        className='block border border-grey-light w-full p-3 rounded mb-4' 
+        type='text' 
+        name='displayName' 
+        value={displayName} 
+        onChange={handleInputChange} />
 
-export default signUpForm
+        <label>Name</label>
+        <input 
+        className='block border border-grey-light w-full p-3 rounded mb-4' 
+        type='text' 
+        name='name' 
+        value={name} 
+        onChange={handleInputChange} />
+
+        <label>email</label>
+        <input 
+        className='block border border-grey-light w-full p-3 rounded mb-4' 
+        type='email' 
+        name='email' 
+        value={email} 
+        onChange={handleInputChange} />
+
+        <label>password</label>
+        <input 
+        className='block border border-grey-light w-full p-3 rounded mb-4' 
+        type='password' 
+        name='password' 
+        value={password} 
+        onChange={handleInputChange} />
+
+        <label>confirm password</label>
+        <input 
+        className='block border border-grey-light w-full p-3 rounded mb-4' 
+        type='password' 
+        name='confirmPassword' 
+        value={confirmPassword} 
+        onChange={handleInputChange} />
+        <button 
+        className='w-full text-center py-3 rounded bg-[orangered] text-white hover:bg-green-dark focus:outline-none my-1'
+        type='submit'>Sign Up</button>
+      </form>
+
+      </div>
+    </div>
+  );
+};
+
+export default SignUpForm;

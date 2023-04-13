10dc5a29234da595c17de56a894a0dc79504f937
fiokee <smleffiong@gmail.com>
2023-04-13 17:05:02 +0100

    adding cartdropdown

---
+++ src/components/cartDrop/CartDrop.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './CartDrop.styles.scss';
+const CartDrop = () => {
+  return (
+    <div className='cart-dropdown-container'>
+      
+    </div>
+  )
+}
+
+export default CartDrop

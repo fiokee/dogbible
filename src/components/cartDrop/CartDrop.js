a020465a8f9cc0a8f0370da6db13fa0987232d40
fiokee <smleffiong@gmail.com>
2023-04-15 16:52:27 +0100

    adding cartitem

--- src/components/cartDrop/CartDrop.js
+++ src/components/cartDrop/CartDrop.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import './CartDrop.styles.scss';
-const CartDrop = () => {
+import CartItem from '../cart-item/CartItem';
+
+const CartDrop = (cartItem) => {
   return (
     <div className='cart-dropdown-container'>
-      <div className='cart-items'/>
+      <div className='cart-items'>
+        <CartItem cartItem={cartItem}/>
+      </div>
       <button>GO TO CHECKOUT</button>
     </div>
   )
--- src/components/contexts/cartContext.js
+++ src/components/contexts/cartContext.js
@@ -2,11 +2,20 @@ import { createContext, useState } from "react";
 
 export const CartContext = createContext({
 isCartOpen: false,
-setIsCartOpen: ()=> {} 
+setIsCartOpen: ()=> {},
+cartItems: [],
+addItemsToCart: ()=>{}
 });
 
 export const CartProvider = ({children})=>{
     const [isCartOpen, setIsCartOpen] = useState(false);
+    const [cartItems, setCartItems] = useState([]);
+
+    const addItemsToCart = (productToAdd)=>{
+
+    }
+
+
     const value= {isCartOpen, setIsCartOpen}
     return(
         <CartContext.Provider value={value}>{children}</CartContext.Provider>

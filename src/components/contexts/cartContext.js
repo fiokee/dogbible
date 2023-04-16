d994655d3ebee812baef22a983aa0562db5cbab1
fiokee <smleffiong@gmail.com>
2023-04-16 15:28:31 +0100

    creating helper function to check and add items to cart

--- src/components/contexts/cartContext.js
+++ src/components/contexts/cartContext.js
@@ -1,5 +1,12 @@
 import { createContext, useState } from "react";
 
+/*An helper function that helps to find any cartitem that exist and match the id of product. 
+    if it finds it increase the quantity otherwise create a new cartitem.
+*/
+const addItems = (cartItems, productToAdd)=>{
+
+}
+
 export const CartContext = createContext({
 isCartOpen: false,
 setIsCartOpen: ()=> {},
@@ -11,6 +18,7 @@ export const CartProvider = ({children})=>{
     const [isCartOpen, setIsCartOpen] = useState(false);
     const [cartItems, setCartItems] = useState([]);
 
+    // A metthode that allow us to add an existing product from product card to the cartItem
     const addItemsToCart = (productToAdd)=>{
 
     }

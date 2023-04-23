9a360f201bfc9a4a6e331c2e0acc0d9bf9372fc8
fiokee <smleffiong@gmail.com>
2023-04-23 16:17:04 +0100

    adding increment functionality for the checkout page

--- src/components/contexts/cartContext.js
+++ src/components/contexts/cartContext.js
@@ -36,7 +36,7 @@ export const CartProvider = ({children})=>{
     //setting the cartCount
 
     useEffect(()=>{
-        const newCartCount = cartItems.reduce((total, cartItem)=> total + cartItem.quantity, 0);
+        const newCartCount = cartItems.reduce((total, cartItem)=> total + cartItem.quantity, 0)
         setCartCount(newCartCount)
     },[cartItems])
 
--- src/routes/checkout/CheckOut.js
+++ src/routes/checkout/CheckOut.js
@@ -2,18 +2,20 @@ import React, {useContext} from 'react'
 import { CartContext } from '../../components/contexts/cartContext';
 
 const CheckOut = () => {
-    const {cartItems}= useContext(CartContext)
+    const {cartItems, addItemsToCart}= useContext(CartContext)
   return (
     <div>
       <h1>Hello Checkout</h1>
       {
-        cartItems.map((cartitem)=>{
-            const {id, name , Price, quantity} = cartitem;
+        cartItems.map((cartItem)=>{
+            const {id, name , quantity} = cartItem;
             return(
             <div key={id}>
                 <h2>{name}</h2>
-                <span>{quantity}</span>
-                <span>{Price}</span>
+                <span>{quantity}</span><br/>
+                <span>Decrement</span> 
+                <br/>
+                <span onClick={()=>addItemsToCart(cartItem)}>increment</span>
             </div>
             )
         })

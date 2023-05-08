7c4b8321aea0e8d589f29ce155e108c7bf1dd9f3
fiokee <smleffiong@gmail.com>
2023-05-08 18:58:27 +0100

    added increment and decrement item functionalty

--- src/components/checkout-item/CheckoutItem.js
+++ src/components/checkout-item/CheckoutItem.js
@@ -1,6 +1,7 @@
 import React, { useContext }from 'react';
 import { CartContext } from '../contexts/cartContext';
-
+import { GrAdd } from 'react-icons/gr';
+import { AiOutlineMinus } from 'react-icons/ai';
 import './checkoutitem.style.scss';
 import { GoTrashcan } from 'react-icons/go';
 
@@ -12,6 +13,8 @@ const CheckoutItem = ({cartitem}) => {
   const {clearItemFromCart , addItemsToCart, removeItemFromCart} = useContext(CartContext);
 
   const clearItemHandler = ()=> clearItemFromCart(cartitem);
+  const removeItemHandler = ()=> removeItemFromCart(cartitem);
+  const addItemHandler = ()=> addItemsToCart(cartitem);
 
   return (
     <div className='checkout-item-container'>
@@ -20,9 +23,9 @@ const CheckoutItem = ({cartitem}) => {
         </div>
         <span className='name'>{name}</span>
         <span className='quantity'>
-          <div className='arrow'>-</div>
+          <div className='arrow' onClick={removeItemHandler}><AiOutlineMinus/></div>
           <span className='value'>{quantity}</span>
-          <div className='arrow'>+</div>
+          <div className='arrow' onClick={addItemHandler}><GrAdd/></div>
           </span>
         <span className='price'>{Price}</span>
         <div className='remove-button' onClick={clearItemHandler}><GoTrashcan/></div>

fdabc5d82c00c7a10fcb91f018edc8be4a8595a2
fiokee <smleffiong@gmail.com>
2023-06-02 20:44:38 +0100

    working on navigation

--- src/routes/navigations/navigation.js
+++ src/routes/navigations/navigation.js
@@ -20,11 +20,11 @@ const Navigation = () => {
 
   return (
     <Fragment>
-      <div className="flex justify-end space-x-4 bg-light-500 my-7 text-xl mr-7">
+      <div className="flex justify-end space-x-4 bg-light-500 my-7 text-xl mr-7 flex-col sm:flex-row list-none lg:ml-auto">
         <Link className="logo-container" to='/'>
         <span className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900 justify-end absolute left-5 text-3xl"><GiSittingDog/></span>
         </Link>
-          <Link className='font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900' to='/'>
+          <Link className='font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900 ' to='/'>
             HOME
           </Link>
           <Link className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900" to='shop'>
